13ac8414767809f8f515c00c554832a52ccdba97
pierrekasongo <pierrekasongo88@gmail.com>
2019-01-29 23:45:29 +0100

    removing some packages
    Removing some usefull packages

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path=require('path');
+const express = require('express')
+
+    ,bodyParser = require('body-parser')
+
+    ,path=require('path')
+
+    ,session =require('express-session')
+
+    ,FileStore = require('session-file-store')(session);
 
 let app = express();
 
@@ -8,14 +14,24 @@ app.set('view engine', 'ejs');
 
 app.set('views', 'views');
 
-const shopRoutes = require('./routes/shop');
+const Routes = require('./routes/myRoutes');
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views/js')));
 
+app.use(session(
+    {
+        name:'beer_bank_session_cookie',
+        secret: 'c@pv66in0',
+        resave: false, // Force save of session for each request.
+        saveUninitialized: false, // Save a session that is new, but has not been modified
+        store: new FileStore(),
+    }
+));
 
-app.use(shopRoutes);
+app.use(Routes);
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
